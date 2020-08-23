36606dbbafd3d7c65df1d97961e91ada2c3b0543
shubheshdwivedi <frostcover@gmail.com>
2020-08-23 07:12:46 +0530

    Initial setup

---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class Timer : MonoBehaviour
+{
+    private bool m_timerState = true;
+
+    [SerializeField] private TextMeshProUGUI m_time;
+    [SerializeField] private float m_timeCounter = 0;
+
+  
+
+    void Update() {
+        if (m_timerState)
+            runTimer();
+
+    }
+
+    void runTimer() {
+        m_timeCounter += Time.deltaTime;
+
+        float minutes = Mathf.FloorToInt(m_timeCounter / 60);
+        float seconds = Mathf.FloorToInt(m_timeCounter % 60);
+
+        m_time.SetText(string.Format("{0:00}:{1:00}", minutes, seconds));
+    }
+
+    public void setTimer(bool timerState) {
+        if (timerState)
+            m_timerState = true;
+        else {
+            m_timerState = false;
+            m_timeCounter = 0;
+        }
+    }
+
+    public void resetTimer() {
+        m_timeCounter = 0;
+    }
+}
